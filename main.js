f543f7f9b5cfd5d9c93c54a76063c79b6ba00989
Josh Howard <jashghoward@icloud.com>
2023-07-28 00:19:54 +0200

    style.css update, animation added

--- main.js
+++ main.js
@@ -35,3 +35,54 @@ function addColor () {
     e.innerHTML = newColor;
   });
 }
+
+function showNotification(box) {
+  const notification = document.querySelector('.notification');
+  notification.style.top = `${box.offsetTop + box.offsetHeight / 2}px`;
+  notification.style.left = `${box.offsetLeft + box.offsetWidth / 2}px`;
+  notification.style.opacity = 1;
+  setTimeout(() => {
+    notification.style.opacity = 0;
+  }, 1000); // Adjust the duration (in milliseconds) as needed
+}
+
+// Function to toggle automatic coloring
+let isPlaying = false;
+let intervalId;
+
+function toggleAutoPlay() {
+  const btn = document.querySelector('.btn');
+  if (isPlaying) {
+    clearInterval(intervalId);
+    isPlaying = false;
+    btn.textContent = 'Play';
+  } else {
+    intervalId = setInterval(addColor, 900); // Adjust the interval (in milliseconds) as desired
+    isPlaying = true;
+    btn.textContent = 'Pause';
+  }
+}
+
+// Add event listener to the "Play" button
+document.querySelector('.btn').addEventListener('click', toggleAutoPlay);
+
+
+
+  // Add the 'flash' class to the clicked box to trigger the animation
+  function flashColorBox(box) {
+    box.classList.add('flash');
+    // Remove the 'flash' class after a short delay to stop the animation
+    setTimeout(() => {
+      box.classList.remove('flash');
+    }, 1000); // Adjust the duration (in milliseconds) to match the animation duration
+  }
+
+  randomColorBlock.forEach(box => {
+    box.addEventListener('click', () => {
+      console.log(box.innerHTML);
+      navigator.clipboard.writeText(box.innerHTML);
+      showNotification(box); // Pass the clicked box element to the showNotification function
+
+      flashColorBox(box); // Trigger the flashing effect
+    });
+  });
